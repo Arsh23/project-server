ba4b3c4182e56c7ebb6c3ea534b63be50bb1f43e
arsh23 <programmer.arsh@gmail.com>
2016-10-29 15:43:56 +0530

    testing auto sudo

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Flask is running with git and supervisor and automated !!!!'
+    return 'Flask is running with git and supervisor and automated and without sudo !!!!'
 
 
 @app.route('/data')
